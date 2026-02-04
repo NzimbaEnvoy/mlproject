51d237b23a12994469d95452c634699af9d5aac3
envoynzimba@gmail.com <envoynzimba@gmail.com>
2026-02-04 16:51:20 +0200

    Fix project packaging and src layout

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = [line.strip() for line in file_obj.readlines() if line.strip()]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Envoy Nzimba",
+    author_email="envoynzimba@gmail.com",
+    package_dir={"": "src"},         
+    packages=find_packages(where="src"),
+    install_requires=get_requirements("requirements.txt"),
+)
